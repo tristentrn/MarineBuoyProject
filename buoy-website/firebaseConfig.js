69f3c2f1777cf918a6d78a24e290cbd814c7ddcf
tristentrn <98061837+tristentrn@users.noreply.github.com>
2026-02-22 16:41:33 -0800

    -fix vercel

---
+++ buoy-website/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyByUS64PQ41yKp1K003DbhaBJ4E_IM1lyo",
+  authDomain: "marine-buoy.firebaseapp.com",
+  databaseURL: "https://marine-buoy-default-rtdb.firebaseio.com",
+  projectId: "marine-buoy",
+  storageBucket: "marine-buoy.firebasestorage.app",
+  messagingSenderId: "126380799821",
+  appId: "1:126380799821:web:2ad49f7dfe0d3fb2f7a904"
+};
